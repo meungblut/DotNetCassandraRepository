6f0756e824113c083d8c795d9e98aeef507bac39
meungblut <meungblut@gmail.com>
2016-06-23 11:52:04 +0100

    readded

---
+++ CassandraConnector.Test/DummyDomainObject.cs
@@ -0,0 +1,11 @@
+﻿namespace CassandraRepository.Test
+{
+    public class DummyDomainObject
+    {
+        public DummyDomainObject(string data)
+        {
+            Data = data;
+        }
+        public string Data { get; }
+    }
+}
---
+++ CassandraRepository/IClusterConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace CassandraRepository
+{
+    public interface IClusterConfiguration
+    {
+        string Password { get; }
+        int Port { get; set; }
+        string Uri { get; }
+        string Username { get; }
+    }
+}
